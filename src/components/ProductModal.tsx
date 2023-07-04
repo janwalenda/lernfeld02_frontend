c4a1ebbc705e274728350149776eabac300b6ede
Jan <log@janwalenda.de>
2023-07-04 14:27:02 +0200

    Nice shit!

---
+++ src/components/ProductModal.tsx
@@ -0,0 +1,5 @@
+export default function ProductModal(): JSX.Element {
+  return (
+    <div></div>
+  );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,40 +1,40 @@
 import MainLayout from '../layouts/Main';
 import PoductCard from '../components/ProductCard';
-import { CardGroup } from './CardGroup';
+import { CardGroup } from '../components/CardGroup';
 
 export default function App() {
   return (
     <MainLayout>
       <CardGroup>
         <PoductCard 
-          productName="Computer 1" 
+          productName="Laptop 1" 
           price={10000} 
           productId="sdgee" 
         />
         <PoductCard 
-          productName="Computer 2" 
+          productName="Laptop 2" 
           price={20} 
           productId="sgeioe" 
         />
         <PoductCard 
-          productName="Computer 3" 
+          productName="Laptop 3" 
           price={5422} 
           productId="nftht" 
         />
       </CardGroup>
       <CardGroup>
         <PoductCard 
-          productName="Computer 4" 
+          productName="Laptop 4" 
           price={100} 
           productId="sdgee" 
         />
         <PoductCard 
-          productName="Computer 5" 
+          productName="Laptop 5" 
           price={.65} 
           productId="sgeioe" 
         />
         <PoductCard 
-          productName="Computer 6" 
+          productName="Laptop 6" 
           price={2343} 
           productId="nftht" 
         />
