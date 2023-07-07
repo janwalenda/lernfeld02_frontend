4ca9a20d72956ab75e3959f9f060efe39c74a5b1
Jan <log@janwalenda.de>
2023-07-07 10:41:39 +0200

    yey

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,9 +4,10 @@ import PoductCard from '../components/ProductCard';
 import styles from '../styles/App.module.scss';
 import { API } from '../classes/API';
 import { useEffect, useState } from 'react';
+import { Product } from '../interfaces/Product';
 
 export default function App() {
-  const [laptops, setLaptops] = useState<any[]>([]);
+  const [laptops, setLaptops] = useState<Product[]>([]);
   
   useEffect(() => {
     const api = new API();
@@ -20,7 +21,7 @@ export default function App() {
     <MainLayout>
       <div className={styles.appPageProductList}>
         {laptops.map(product => {
-          return <PoductCard {...product} key={product.productId} />
+          return <PoductCard {...product} key={product.id} />
         })}
       </div>
     </MainLayout>
