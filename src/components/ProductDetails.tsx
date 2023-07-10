f6dc49f946e38c918a9f3ab10231fc0e18000591
Jan <log@janwalenda.de>
2023-07-10 09:28:06 +0200

    formatted html

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -2,18 +2,38 @@ import { Product } from "../interfaces/Product";
 
 export default function ProductDetails(item: Product) {
   return (
-    <aside >
+    <div>
       <div >
         <h4>Details</h4>
       </div>
-      <small>Name: {item.name}</small><br/>
-      <small>CPU: {item.cpu.name}</small><br/>
-      <small>Cores: {item.cpu.count_amount}</small><br/>
-      <small>Display Size: {item.display.size_in_zoll} Zoll</small><br/>
-      <small>Resolution: {item.display.resolution_in_pixel} Pixel</small><br/>
-      <small>Battery: {item.battery.capacity_in_wh} wh</small><br/>
-      <small>RAM: {item.ram.capacity_in_gb} GB</small><br/>
-      <small>Describtion: {item.description}</small><br/>
-    </aside>
+      <small>
+        <h5>CPU:</h5>
+        <p>{item.cpu.name}</p>
+      </small>
+      <small>
+        <h5>Cores:</h5>
+        <p>{item.cpu.count_amount}</p>
+      </small>
+      <small>
+        <h5>Display Size:</h5>
+        <p>{item.display.size_in_zoll} Zoll</p>
+      </small>
+      <small>
+        <h5>Resolution:</h5>
+        <p>{item.display.resolution_in_pixel} Pixel</p>
+      </small>
+      <small>
+        <h5>Battery:</h5>
+        <p>{item.battery.capacity_in_wh} wh</p>
+      </small>
+      <small>
+        <h5>RAM:</h5>
+        <p>{item.ram.capacity_in_gb} GB</p>
+      </small>
+      <small>
+        <h5>Describtion:</h5>
+        <p>{item.description}</p>
+      </small>
+    </div>
   );
 }
